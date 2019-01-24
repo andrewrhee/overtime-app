9bde850997a11602a51e438f97e68f85bae78c03
Andrew Rhee <adrhee@gmail.com>
2019-01-23 16:45:23 -0800

    Updated the audit log factory and tested out six day default

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,11 @@ puts "1 Admin User Created"
 end
 
 puts "100 Posts have been created"
+
+100.times do |audit_log|
+  AuditLog.create!(user_id: User.last.id,
+        status: 0,
+        start_date: (Date.today - 6.days))
+end
+
+puts "100 Audit Logs have been created"
